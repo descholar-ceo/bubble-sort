a30c1fdcdfd23e05f96e2162da041edac78d398b
descholar-ceo <emmanuellamugi@gmail.com>
2020-07-14 10:38:14 +0200

    add sort first method

--- sort.rb
+++ sort.rb
@@ -1,1 +1,19 @@
-puts 'hello world'
+def sort(array)
+  c = 0
+  (0..array.length - 1).each do
+    array[0...-1].each.with_index do |_, i|
+      left = array[i]
+      right = array[i + 1]
+      if left > right
+        array[i] = right
+        array[i + 1] = left
+        c += 1
+      end
+    end
+    if c.zero?
+      puts "\n !!! No sort condition #{array}\n\n"
+      return array
+    end
+  end
+  array
+end
